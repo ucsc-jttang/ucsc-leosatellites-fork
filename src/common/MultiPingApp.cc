ded27eec6171b4382d8486bbd1f191e373502d11
ucsc-jttang <jttang@ucsc.edu>
2024-11-11 22:18:49 -0300

    MultiPingApp intended for the 46 base stations

--- src/common/MultiPingApp.cc
+++ src/common/MultiPingApp.cc
@@ -130,46 +130,43 @@ void MultiPingApp::parseDestAddressesPar()
 {
     srcAddr = L3AddressResolver().resolve(par("srcAddr"));
     const char *destAddrs = par("destAddr");
-    int selfId = par("selfId");
-    int startRange = par("startRange");
-    int endRange = par("endRange");
 
-    std::string temp = "";
-    if (!strcmp(destAddrs, "intf")) {
-        destAddresses=getAllAddresses();
-        std::hash<std::string> hasher;
-        long hashed = hasher(seed.c_str());
-        std::mt19937 rng;
-        rng.seed(hashed);
-        std::shuffle(destAddresses.begin(),destAddresses.end(),rng);
-
-    }
-    else if (!strcmp(destAddrs, "gs")) {
-        for(int i =startRange; i<=endRange;i++){
-            if(i != selfId){
-                temp += "shell[0].groundStation[" + std::to_string(i) + "] ";
-            }
-        }
-        cStringTokenizer tokenizer(temp.c_str());
+//    std::string temp = "";
+//    if (!strcmp(destAddrs, "intf")) {
+//        destAddresses=getAllAddresses();
+//        std::hash<std::string> hasher;
+//        long hashed = hasher(seed.c_str());
+//        std::mt19937 rng;
+//        rng.seed(hashed);
+//        std::shuffle(destAddresses.begin(),destAddresses.end(),rng);
+//
+//    }
+//    else if (!strcmp(destAddrs, "gs")) {
+//        for(int i =startRange; i<=endRange;i++){
+//            if(i != selfId){
+//                temp += "shell[0].groundStation[" + std::to_string(i) + "] ";
+//            }
+//        }
+//        cStringTokenizer tokenizer(temp.c_str());
+//        const char *token;
+//        while ((token = tokenizer.nextToken()) != nullptr) {
+//            L3Address addr = L3AddressResolver().resolve(token);
+//            destAddresses.push_back(addr);
+//            std::mt19937 rng;
+//            std::hash<std::string> hasher;
+//            long hashed = hasher(seed.c_str());
+//            rng.seed(hashed);
+//            std::shuffle(destAddresses.begin(),destAddresses.end(),rng);
+//        }
+//    }
+//    else {
+        cStringTokenizer tokenizer(destAddrs);
         const char *token;
         while ((token = tokenizer.nextToken()) != nullptr) {
             L3Address addr = L3AddressResolver().resolve(token);
             destAddresses.push_back(addr);
-            std::mt19937 rng;
-            std::hash<std::string> hasher;
-            long hashed = hasher(seed.c_str());
-            rng.seed(hashed);
-            std::shuffle(destAddresses.begin(),destAddresses.end(),rng);
         }
-    }
-    else {
-        cStringTokenizer tokenizer(temp.c_str());
-        const char *token;
-        while ((token = tokenizer.nextToken()) != nullptr) {
-            L3Address addr = L3AddressResolver().resolve(token);
-            destAddresses.push_back(addr);
-        }
-    }
+//    }
 }
 
 void MultiPingApp::handleSelfMessage(cMessage *msg)
